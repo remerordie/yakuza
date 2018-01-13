fbace53a072fc83e58a9e700919d51f1c41d91b9
remerordie <remerotg@gmail.com>
2018-01-13 15:40:22 -0600

    no message

---
+++ Assets/SCRIPTS/CanTeleInteract.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanTeleInteract : MonoBehaviour {
+
+	//Objects that the player can use the tele skill on derive from this class
+	//Provie the Game manager script with a list of the objects that the player
+	//can manipulate on screen on scene awake()
+
+
+}
